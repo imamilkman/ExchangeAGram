6a9982dd4423c6f624fca5a5a6e4495fe6d8580e
imamilkman <mmspratt@icloud.com>
2015-08-05 16:44:15 -0500

    added feedcell class and 2 elements in storyboard

---
+++ FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Michael Spratt on 8/5/15.
+//  Copyright (c) 2015 Technolopy. All rights reserved.
+//
+
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+}
